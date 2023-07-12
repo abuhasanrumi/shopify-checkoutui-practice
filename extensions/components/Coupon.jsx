15d60d77cf35120cf202f93e2db9284e5fb7fe5e
Abid Hasan <abid@devsnest.net>
2023-07-12 12:50:51 +0600

    modified coupon

--- extensions/components/Coupon.jsx
+++ extensions/components/Coupon.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import { InlineLayout, Icon, View, Text, Grid, Button, useApplyDiscountCodeChange } from '@shopify/checkout-ui-extensions-react'
 
-export default function Coupon() {
+export default function Coupon({ data }) {
+  const couponCode = JSON.parse(data?.widgets?.at(0)?.widget_customization)?.title?.coupon
   const [loading, setLoading] = useState(false)
   const applyDiscountCodeChange = useApplyDiscountCodeChange()
 
-  return (
+  if (!couponCode) return <></>
+  else return (
     <InlineLayout border='base' borderWidth='base' borderRadius='base' padding='base'>
       <View>
         <Grid columns={['auto', 'fill', 'fill']} spacing='base' blockAlignment='center'>
           <Icon size='large' appearance='subdued' source='discount' />
-          <Text emphasis='bold' appearance='subdued'>WINTERSALE</Text>
+          <Text emphasis='bold' appearance='subdued'>{couponCode}</Text>
           <View inlineAlignment='end'>
             <Button
               loading={loading}
               onPress={() => {
                 setLoading(true)
-                applyDiscountCodeChange({ type: 'addDiscountCode', code: 'WINTERSALE' })
+                applyDiscountCodeChange({ type: 'addDiscountCode', code: couponCode })
                   .finally(() => setLoading(false))
               }}
             >Apply</Button>
