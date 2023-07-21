54b28bfd11d4c1380d7b3e0e4d91cd5513866559
Abid Hasan <abid@devsnest.net>
2023-07-21 11:08:48 +0600

    added timer

--- extensions/components/ListProduct.jsx
+++ extensions/components/ListProduct.jsx
@@ -2,11 +2,25 @@ import { BlockLayout, Heading, Grid, Text, Image, View, Button, TextBlock, useEx
 import { useEffect, useState } from "react";
 
 export default function ListProduct({ data }) {
+  const fixedTime = JSON.parse(data?.widgets?.at(0)?.widget_customization)?.time
   const { i18n } = useExtensionApi()
   const applyCartLineChange = useApplyCartLinesChange()
 
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState({})
+  const [time, setTime] = useState(fixedTime)
+
+  useEffect(() => {
+    if (fixedTime) {
+      const timer = setInterval(() => setTime(state => state - 1), 1000)
+      const timerCleaner = setTimeout(() => clearInterval(timer), ((fixedTime + 2) * 1000))
+
+      return () => {
+        clearInterval(timer)
+        clearTimeout(timerCleaner)
+      }
+    }
+  }, [])
 
   useEffect(() => {
     const tempProducts = []
@@ -19,7 +33,8 @@ export default function ListProduct({ data }) {
     setLoading(tempLoading)
   }, [data])
 
-  return (
+  if (time <= 0) return <></>
+  else return (
     <ScrollView hint={{ type: 'pill', content: 'Scroll to view' }} maxBlockSize={500}>
       <BlockLayout>
         <View>
@@ -32,38 +47,36 @@ export default function ListProduct({ data }) {
             {
               products.map(product => {
                 return (
-                  <>
-                    <Grid border='base' borderWidth='base' borderRadius='base' key={product?.node?.id} blockAlignment='center' columns={['30%', 'fill', '30%']}>
-                      <View padding='base'>
-                        <Image aspectRatio={1.5} fit='contain' source={product?.node?.featuredImage?.url} />
-                      </View>
-                      <View>
-                        <TextBlock size="small">{product?.node?.title}</TextBlock>
-                        <Text appearance="subdued" size="small">{i18n.formatCurrency(product?.node?.variants?.nodes?.at(0)?.price)}</Text>
-                      </View>
-                      <View inlineAlignment='center'>
-                        <Button
-                          loading={loading[product?.node?.variants?.nodes?.at(0)?.id]}
-                          onPress={async () => {
-                            const oldLoading = structuredClone(loading)
-                            oldLoading[product?.node?.variants?.nodes?.at(0)?.id] = true
-                            setLoading(oldLoading)
+                  <Grid border='base' borderWidth='base' borderRadius='base' key={product?.node?.id + 'NavidiumListProduct'} blockAlignment='center' columns={['30%', 'fill', '30%']}>
+                    <View padding='base'>
+                      <Image aspectRatio={1.5} fit='contain' source={product?.node?.featuredImage?.url} />
+                    </View>
+                    <View>
+                      <TextBlock size="small">{product?.node?.title}</TextBlock>
+                      <Text appearance="subdued" size="small">{i18n.formatCurrency(product?.node?.variants?.nodes?.at(0)?.price)}</Text>
+                    </View>
+                    <View inlineAlignment='center'>
+                      <Button
+                        loading={loading[product?.node?.variants?.nodes?.at(0)?.id]}
+                        onPress={async () => {
+                          const oldLoading = structuredClone(loading)
+                          oldLoading[product?.node?.variants?.nodes?.at(0)?.id] = true
+                          setLoading(oldLoading)
 
-                            try {
-                              applyCartLineChange({
-                                type: 'addCartLine',
-                                quantity: 1,
-                                merchandiseId: product?.node?.variants?.nodes?.at(0)?.id
-                              })
-                            } finally {
-                              oldLoading[product?.node?.variants?.nodes?.at(0)?.id] = false
-                              setLoading(oldLoading)
-                            }
-                          }}
-                        >Add to cart</Button>
-                      </View>
-                    </Grid>
-                  </>
+                          try {
+                            applyCartLineChange({
+                              type: 'addCartLine',
+                              quantity: 1,
+                              merchandiseId: product?.node?.variants?.nodes?.at(0)?.id
+                            })
+                          } finally {
+                            oldLoading[product?.node?.variants?.nodes?.at(0)?.id] = false
+                            setLoading(oldLoading)
+                          }
+                        }}
+                      >Add to cart</Button>
+                    </View>
+                  </Grid>
                 )
               })
             }
