83eee3691ad58fdf276ad8c7b7618b8a32ff8404
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-10-10 12:17:10 +0600

    product adding to cart

--- extensions/single-product-carousel/src/index.jsx
+++ extensions/single-product-carousel/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import {
   useExtensionApi,
+  useApplyCartLinesChange,
   render,
   BlockLayout,
   BlockStack,
@@ -19,6 +22,58 @@ render('Checkout::Dynamic::Render', () => <App />)
 
 function App() {
   const { extensionPoint } = useExtensionApi()
+  const templateId = 33
+  const [widgetData, setWidgetData] = useState('')
+
+  const fetchUrl =
+    'https://tom-iron-tracked-chambers.trycloudflare.com/api/dynamatic/checkout-info?shop=mehedi-test-store.myshopify.com&user-email=khmehedi.dev.llc@gmail.com&products=&widget-position=purchase.checkout.block.render&user-phone=undefined'
+
+  useEffect(() => {
+    fetch(fetchUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        data?.campaigns[0]?.widgets?.map((widget) => {
+          // console.log(widget)
+          if (widget?.template == templateId) {
+            // console.log(widget)
+            setWidgetData(widget)
+          }
+        })
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error)
+      })
+  }, [])
+
+  function getRandomItemFromArray(array) {
+    if (Array.isArray(array) && array.length > 0) {
+      const randomIndex = Math.floor(Math.random() * array.length)
+      return array[randomIndex]
+    } else {
+      return null
+    }
+  }
+
+  const productsArray =
+    widgetData?.products_in_widget?.products[0]?.product_variants
+  const randomlySelectedProduct = getRandomItemFromArray(productsArray)
+
+  console.log(randomlySelectedProduct)
+
+  const applyCartLinesChange = useApplyCartLinesChange()
+  const onClickAddToCart = async (product) => {
+    let result = await applyCartLinesChange({
+      type: 'addCartLine',
+      merchandiseId: `gid://shopify/ProductVariant/${product.variant_id}`,
+      quantity: 1
+    })
+  }
+
   return (
     <BlockLayout
       rows={[20, 'fill']}
@@ -30,34 +85,40 @@ function App() {
       <View>
         <Text size={'extraLarge'}>YOU MAY ALSO LIKE</Text>
       </View>
-      <Grid
-        columns={['30%', 'fill', 'auto']}
-        rows={['auto']}
-        blockAlignment='center'
-        spacing='base'>
-        <View>
-          <Image
-            cornerRadius='base'
-            source='https://navidiumapp.net/dynamatic-v2/public/build/assets/shoe_1-84c0649c.png'
-          />
-        </View>
-        <View>
-          <Heading size={'large'} spacing='loose'>
-            Living room sofa
-          </Heading>
-          <BlockSpacer spacing='base' />
-          <Text size={'medium'}>$80.00</Text>
-        </View>
+      {randomlySelectedProduct != null && (
+        <Grid
+          columns={['30%', 'fill', 'auto']}
+          rows={['auto']}
+          blockAlignment='center'
+          spacing='base'>
+          <View>
+            <Image
+              cornerRadius='base'
+              source={
+                widgetData?.products_in_widget?.products[0]?.product_image
+              }
+            />
+          </View>
+          <View>
+            <Heading size={'large'} spacing='loose'>
+              {randomlySelectedProduct?.display_name}
+            </Heading>
+            <BlockSpacer spacing='base' />
+            <Text size={'medium'}>
+              {`$${randomlySelectedProduct?.variant_price}`}
+            </Text>
+          </View>
 
-        <View>
-          <Button
-            onPress={() => {
-              console.log('onPress event')
-            }}>
-            + Add
-          </Button>
-        </View>
-      </Grid>
+          <View>
+            <Button
+              onPress={() => {
+                onClickAddToCart(randomlySelectedProduct)
+              }}>
+              {widgetData?.property?.button_text}
+            </Button>
+          </View>
+        </Grid>
+      )}
     </BlockLayout>
   )
 }
