6d20cadaf37631b3e47219932f091b7a7e925657
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-10-11 12:04:10 +0600

    widget primary functionalities complete

--- extensions/single-product-carousel/src/index.jsx
+++ extensions/single-product-carousel/src/index.jsx
@@ -23,17 +23,20 @@ render('Checkout::Dynamic::Render', () => <App />)
 
 function App() {
   const { extensionPoint } = useExtensionApi()
+
   const templateId = 31
+  const fetchUrl =
+    'https://orange-anthony-jones-vintage.trycloudflare.com/api/dynamatic/checkout-info?shop=mehedi-test-store.myshopify.com&user-email=khmehedi.dev.llc@gmail.com&products=&widget-position=purchase.checkout.block.render&user-phone=undefined'
+
   const [widgetData, setWidgetData] = useState('')
   const [added, setAdded] = useState(false)
   const [loading, setLoading] = useState(false)
   const [btnLoading, setBtnLoading] = useState(false)
   const [products, setProducts] = useState(false)
+  const [currentProductIndex, setCurrentProductIndex] = useState(0)
+  const [variantId, setVariantId] = useState('')
   const applyCartLinesChange = useApplyCartLinesChange()
   const cartLines = useCartLines()
-
-  const fetchUrl =
-    'https://tom-iron-tracked-chambers.trycloudflare.com/api/dynamatic/checkout-info?shop=mehedi-test-store.myshopify.com&user-email=khmehedi.dev.llc@gmail.com&products=&widget-position=purchase.checkout.block.render&user-phone=undefined'
 
   useEffect(() => {
     setLoading(true)
@@ -46,7 +49,7 @@ function App() {
       })
       .then((data) => {
         data?.campaigns[0]?.widgets?.map((widget) => {
-          if (widget?.template == templateId) {
+          if (widget?.template === templateId) {
             setWidgetData(widget)
             setProducts(widget?.products_in_widget?.products)
           }
@@ -59,7 +62,18 @@ function App() {
       })
   }, [])
 
+  // Declare currentDisplayedProduct
+  const currentDisplayedProduct = products[currentProductIndex]
+
+  const options = currentDisplayedProduct?.product_variants.map(
+    (variant, index) => ({
+      value: `${variant.variant_id}`,
+      label: `$${variant.variant_price}`
+    })
+  )
+
   function getGid(id) {
+    console.log('getgid', id)
     return `gid://shopify/ProductVariant/${id}`
   }
 
@@ -70,34 +84,6 @@ function App() {
     return cartLine?.id || ''
   }
 
-  const onClickAddToCart = async (product) => {
-    setBtnLoading(true)
-    const merchandiseId = getGid(product.variant_id)
-    const result = await applyCartLinesChange({
-      type: 'addCartLine',
-      merchandiseId,
-      quantity: 1
-    })
-    setAdded(true)
-    setBtnLoading(false)
-  }
-
-  const onClickRemoveFromCart = async (product) => {
-    setBtnLoading(true)
-    const variantId = getGid(product?.variant_id)
-    const cartLineId = getCartLineItemId(variantId)
-    const result = await applyCartLinesChange({
-      type: 'removeCartLine',
-      id: getGid(cartLineId),
-      quantity: 1
-    })
-    setAdded(false)
-    setBtnLoading(false)
-  }
-
-  const [currentProductIndex, setCurrentProductIndex] = useState(0)
-
-  // Function to handle clicking the right arrow button
   const handleNextProduct = () => {
     if (products?.length > 0) {
       const nextIndex = (currentProductIndex + 1) % products.length
@@ -105,7 +91,6 @@ function App() {
     }
   }
 
-  // Function to handle clicking the left arrow button
   const handlePrevProduct = () => {
     if (products?.length > 0) {
       const prevIndex =
@@ -116,82 +101,99 @@ function App() {
     }
   }
 
-  const currentDisplayedProduct = products[currentProductIndex]
+  const onClickAddToCart = async (product) => {
+    setBtnLoading(true)
+    let merchandiseId = ''
+    if (variantId) {
+      merchandiseId = getGid(variantId)
+    } else {
+      const selectedVariant = product?.product_variants[0]
+      merchandiseId = getGid(selectedVariant?.variant_id)
+    }
 
-  const options = currentDisplayedProduct?.product_variants.map(
-    (variant, index) => ({
-      value: `variant-${index}`,
-      label: `$${variant.variant_price}`
-    })
-  )
+    if (merchandiseId) {
+      console.log(merchandiseId)
+      const result = await applyCartLinesChange({
+        type: 'addCartLine',
+        merchandiseId,
+        quantity: 1
+      })
+      setAdded(true)
+      setVariantId('')
+    }
+
+    setBtnLoading(false)
+  }
 
   return (
-    <BlockLayout
-      rows={['auto', 'fill']}
-      border='base'
-      cornerRadius='base'
-      padding='base'
-      spacing='large200'
-      borderRadius='10px'>
-      <View>
-        <Text size={'extraLarge'}>You may also like</Text>
-      </View>
-      {loading && (
-        <Grid
-          columns={['5%', '20%', 'fill', 'auto', '5%']}
-          rows={['auto']}
-          blockAlignment='center'
-          spacing='base'>
-          <SkeletonImage inlineSize={10} blockSize={20} />
-          <SkeletonImage inlineSize={100} blockSize={100} />
-          <View>
-            <SkeletonImage inlineSize={150} blockSize={20} />
-            <BlockSpacer spacing='base' />
-            <SkeletonText />
-          </View>
-          <SkeletonImage inlineSize={100} blockSize={35} />
-          <SkeletonImage inlineSize={10} blockSize={20} />
-        </Grid>
-      )}
-      {currentDisplayedProduct != null && !loading && (
-        <Grid
-          columns={['5%', '20%', 'fill', 'auto', '5%']}
-          rows={['auto', 'auto']}
-          blockAlignment='center'
-          spacing='base'>
-          <Button appearance='accent' onPress={() => handlePrevProduct()}>
-            <Icon appearance='interactive' source='chevronLeft' />
-          </Button>
-          <View>
-            <Image
-              cornerRadius='base'
-              source={currentDisplayedProduct?.product_image}
-            />
-          </View>
-          <View>
-            <Heading size={'large'} spacing='loose'>
-              {currentDisplayedProduct?.product_title}
-            </Heading>
-            <BlockSpacer spacing='base' />
+    !added && (
+      <BlockLayout
+        rows={['auto', 'fill']}
+        border='base'
+        cornerRadius='base'
+        padding='base'
+        spacing='large200'
+        borderRadius='10px'>
+        <View>
+          <Text size={'extraLarge'}>You may also like</Text>
+        </View>
+        {loading && (
+          <Grid
+            columns={['5%', '20%', 'fill', 'auto', '5%']}
+            rows={['auto']}
+            blockAlignment='center'
+            spacing='base'>
+            <SkeletonImage inlineSize={10} blockSize={20} />
+            <SkeletonImage inlineSize={100} blockSize={100} />
             <View>
-              {console.log(currentDisplayedProduct?.product_variants)}
-              {currentDisplayedProduct?.product_variants?.length == 1 ? (
-                <Text
-                  size={
-                    'medium'
-                  }>{`$${currentDisplayedProduct?.product_variants[0]?.variant_price}`}</Text>
-              ) : (
-                <Select
-                  label='Variants'
-                  value={options[0].value}
-                  options={options}
-                />
-              )}
+              <SkeletonImage inlineSize={150} blockSize={20} />
+              <BlockSpacer spacing='base' />
+              <SkeletonText />
             </View>
-          </View>
+            <SkeletonImage inlineSize={100} blockSize={35} />
+            <SkeletonImage inlineSize={10} blockSize={20} />
+          </Grid>
+        )}
+        {currentDisplayedProduct != null && !loading && (
+          <Grid
+            columns={['5%', '20%', 'fill', 'auto', '5%']}
+            rows={['auto', 'auto']}
+            blockAlignment='center'
+            spacing='base'>
+            <Button appearance='accent' onPress={() => handlePrevProduct()}>
+              <Icon appearance='interactive' source='chevronLeft' />
+            </Button>
+            <View>
+              <Image
+                cornerRadius='base'
+                source={currentDisplayedProduct?.product_image}
+              />
+            </View>
+            <View>
+              <Heading size={'large'} spacing='loose'>
+                {currentDisplayedProduct?.product_title}
+              </Heading>
+              <BlockSpacer spacing='base' />
+              <View>
+                {currentDisplayedProduct?.product_variants?.length == 1 ? (
+                  <Text
+                    size={
+                      'medium'
+                    }>{`$${currentDisplayedProduct?.product_variants[0]?.variant_price}`}</Text>
+                ) : (
+                  <Select
+                    label='Variants'
+                    value={variantId}
+                    options={options}
+                    onChange={(e) => {
+                      setVariantId(e)
+                    }}
+                  />
+                )}
+              </View>
+            </View>
 
-          <View>
-            {added == false ? (
+            <View>
               <Button
                 loading={btnLoading}
                 onPress={() => {
@@ -199,22 +201,13 @@ function App() {
                 }}>
                 {widgetData?.property?.button_text}
               </Button>
-            ) : (
-              <Button
-                loading={btnLoading}
-                appearance='critical'
-                onPress={() => {
-                  onClickRemoveFromCart(currentDisplayedProduct)
-                }}>
-                Remove
-              </Button>
-            )}
-          </View>
-          <Button appearance='accent' onPress={() => handleNextProduct()}>
-            <Icon appearance='interactive' source='chevronRight' />
-          </Button>
-        </Grid>
-      )}
-    </BlockLayout>
+            </View>
+            <Button appearance='accent' onPress={() => handleNextProduct()}>
+              <Icon appearance='interactive' source='chevronRight' />
+            </Button>
+          </Grid>
+        )}
+      </BlockLayout>
+    )
   )
 }
